622b794d8dc849e75bdf196167f4ca0e91c895b2
VICTOR FERREIRA DO VALE <victor.vale@uvvnet.com.br>
2021-10-18 09:44:11 -0300

    1o exercicio

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,98 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+const array = ["verde", "azul", "amarelo"]
+function retornaTamanhoArray(array) {
+    const tamanho = array.length
+    return tamanho
+}
+const num = retornaTamanhoArray(array)
+console.log(num)
+
+// EXERCÍCIO 02
+const array = ["gato", "cachorro", "galinha", "passaro"]
+
+function retornaArrayInvertido(array) {
+    const invertido = array.reverse()
+    return invertido
+}
+const num = retornaArrayInvertido(array)
+console.log(num)
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  
+}
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+  
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
