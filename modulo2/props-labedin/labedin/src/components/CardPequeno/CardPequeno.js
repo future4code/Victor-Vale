3451bcb38cc8dbf3c68636194ae120aab23a514e
Your Name <you@example.com>
2021-11-07 10:24:57 -0300

    insta 4a

--- modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo2/props-labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './CardPequeno.css'
+import {card1} from './styles'
 
 function CardPequeno(props) {
     return (
-        <div className="cardpequeno-conteiner">
+        <div>
             <img src={ props.imagem }/>
             <p>{ props.texto }</p>
         </div>
