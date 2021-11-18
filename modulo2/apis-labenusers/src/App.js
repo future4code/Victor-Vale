22d065c82c94336e37d1ba7d904dae9cae69ffca
Your Name <you@example.com>
2021-11-18 16:16:12 -0300

    Exercicio Integração de API finalizado

--- modulo2/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/src/App.js
@@ -1,81 +1,37 @@
-// EXERCICIO CARTÃO DE CRÉDITO MOE
 
 import React from "react"
 import axios from "axios"
+import TelaCadastro from "./Components/TelaCadastro"
+import TelaListaUsuarios from "./Components/TelaListaUsuarios"
 
-export default class MeuComponente extends React.Component{
+export default class App extends React.Component{
   state = {
-    usuarios: [],
-    inputValueNome:"",
-    inputValueEmail:""
+    telaAtual: "cadastro"
 
   }
-  handleInputChangeNome = (event) => {
-    this.setState({inputValueNome: event.target.value})
-  }
-  handleInputChangeEmail = (event) => {
-    this.setState({inputValueEmail: event.target.value})
-  }
-
-  getUsuarios = () => {
-    axios
-      .get(
-        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        {
-          headers: {
-            Authorization: "victor-vale-carver"
-          }
-        }
-      )
-      .then((res) => {
-        this.setState ({usuarios: res.data.resul.list})
-        console.log(res.data.result.list)
-      })
-      .catch((err) => {
-        console.log(err.response.data)
-      })
-  }
-  
-  onClickCriar = () => {
-    const body = {
-      name: this.state.inputValueNome,
-      email: this.state.inputValueEmail
+  escolheTela = () => {
+    switch(this.state.telaAtual){
+      case "cadastro":
+        return <TelaCadastro irParaLista = {this.irParaLista} />
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro = {this.irParaCadastro}/>
+      default:
+        return <div>Página não encontrada</div>
     }
-    axios
-      .post(
-        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        body,
-        {
-          headers: {
-            Authorization: "victor-vale-carver"
-          }
-        }
-      )
-      .then((res) => {
-        this.setState({inputValueNome: ""})
-        this.setState({inputValueEmail: ""})
-        this.getUsuarios()
-        alert("usuário adicionado com sucesso!")
-      })
-      .catch((err) => {
-        console.log(err.response.data)
-      })
+  }
+  irParaCadastro = () => {
+    this.setState({telaAtual: "cadastro"})
+  }
+
+  irParaLista = () => {
+    this.setState({telaAtual: "lista"})
   }
   
   render(){
-    const listaUsuarios = this.state.usuarios.map((x) => {
-      <li key={x.id}>{x.name}</li>
-    })
-    return(
+    return (
       <div>
-        <div>
-          <input placeholder = "nome" value = {this.state.inputValueNome} onChange = {this.handleInputChangeNome}/>
-          <input placeholder = "e-mail" value = {this.state.inputValueEmail} onChange = {this.handleInputChangeEmail}/>
-          <button onClick = {this.onClickCriar}>enviar</button>
-        </div>
-        <ul>{listaUsuarios}</ul>
+        {this.escolheTela()}
       </div>
-
-    )
+    )  
   }
 }
