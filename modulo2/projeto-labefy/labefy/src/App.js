39e316269c5e9370f94b69f15bf906963eda6f6c
Your Name <you@example.com>
2021-11-19 21:14:43 -0300

    a função de pegar informações já funciona

--- modulo2/projeto-labefy/labefy/src/App.js
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -1,10 +1,11 @@
 import React from "react"
 import axios from "axios"
 
-export default class TelaCadastro extends React.Component{
+export default class TelaPlaylists extends React.Component{
     state = {
             playlists: [],
-            nome: ""
+            nome: "",
+            detalhes:[]
         }
 
         componentDidMount() {
@@ -13,6 +14,21 @@ export default class TelaCadastro extends React.Component{
 
         handleNome = (event) => {
             this.setState ({nome: event.target.value})
+        }
+
+        pegarPlaylists = () => {
+            const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+            axios.get(url, {
+                headers:{
+                    Authorization: "victor-vale-carver"
+                }
+            })
+            .then((res) => {
+                this.setState({playlists: res.data.result.list})
+            })
+            .catch((err) => {
+                alert("ocorreu um problema, tente novamente")
+            })
         }
        
         criarPlaylist = () => {
@@ -26,8 +42,8 @@ export default class TelaCadastro extends React.Component{
                 }
             })
             .then((res)=>{
-                console.log(res)
                 alert("playlist criada!")
+                this.pegarPlaylists()
             })
             .catch((err)=>{
                 console.log(err.response.data)
@@ -37,30 +53,57 @@ export default class TelaCadastro extends React.Component{
             this.setState({nome: ""})
         }
 
-        pegarPlaylists = () => {
-          const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
-          axios.get(url, {
-              headers:{
-                  Authorization: "victor-vale-carver"
-              }
-          })
-          .then((res) => {
-              this.setState({playlists: res.data})
-          })
-          .catch((err) => {
-              alert("ocorreu um problema, tente novamente")
-          })
-      }
+        deletarPlaylist = (id) => {
+            const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}`
+
+            axios.delete(url,{
+                headers:{
+                    Authorization: "victor-vale-carver"
+                }
+            })
+            .then((res) => {
+                alert("Playlist deletada")
+                this.pegarPlaylists()
+            })
+            .catch((err) => {
+                alert("ocorreu um erro")
+            })
+        }
+
+        pegarInformacoes = (id) => {
+            const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
+            axios.get(url, {
+                headers:{
+                    Authorization: "victor-vale-carver"
+                }
+            }).then((res) => {
+                this.setState({detalhes: res.data})
+                console.log(res.data)
+            }).catch((err) => {
+                console.log(err)
+            }) 
+        }
+       
 
     
     render(){
-        console.log(this.state.playlists)
+        console.log("playlists", this.state.playlists)
+        const listaPlaylists = this.state.playlists.map((x) => {
+            return <div key={x.id}>
+                {x.name}
+                <button onClick={() => this.deletarPlaylist(x.id)}>Deletar Playlist</button>
+                <button onClick = {() => this.pegarInformacoes(x.id)}> Ver informações </button>
+                <button>Adicionar Músicas</button> 
+                </div>
+        })
         
-
         return (
             <div>
                 <input placeholder = "Nome da playlist" value = {this.state.nome} onChange = {this.handleNome}></input>
-                <button onClick = {this.criarPlaylist}>Cadastrar</button> 
+                <button onClick = {this.criarPlaylist}>Criar</button> 
+                <h2>Lista de Playlists</h2>
+                <h4>{listaPlaylists}</h4>
+
             </div>
         )
     }
