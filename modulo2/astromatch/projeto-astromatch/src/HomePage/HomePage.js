e5bc6035216cdea52e198c734ac96a24e3b34d28
VICTOR FERREIRA DO VALE <victor.vale@uvvnet.com.br>
2021-12-01 21:47:44 -0300

    trabalho astromatch

--- modulo2/astromatch/projeto-astromatch/src/HomePage/HomePage.js
+++ modulo2/astromatch/projeto-astromatch/src/HomePage/HomePage.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 
-export default class HomePage extends React.Component {
- 
-  render() {
+
+export default function HomePage() {
+    const [id, setId] = useState('')
+    const [idade, setIdade] = useState('')
+    const [nome, setNome] = useState('')
+    const [foto, setFoto] = useState('')
+    const [bio, setBio] = useState('')
+    
+    useEffect(() => {
+        getProfiles();
+      }, [id]);
+
+    const getProfiles = () => {
+        axios.get ('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:victor/person')
+        .then((res) => {
+            setIdade(res.data.profile.age)
+            setNome(res.data.profile.name)
+            setFoto(res.data.profile.photo)
+            setBio(res.data.profile.bio)
+            
+        })
+        .catch((err)=> {
+            console.log("deu erro coisinha")
+        })
+        
+    }
+
     return (
       <div>
         HomePage
       </div>
     )
-  }
+  
 
 }
     
