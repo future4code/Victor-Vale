98a4211d2303137d518b7db6faa00e3ff9994ed8
VICTOR FERREIRA DO VALE <vferreiradovale@gmail.com>
2022-01-21 20:23:21 -0300

    lista

--- modulo4/aprofundamento-typescript/src/index.ts
+++ modulo4/aprofundamento-typescript/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Boa noite lindos')
+function checaAnoBissexto(ano: number) {
+    const cond1 = ano % 400 === 0
+    const cond2  = (ano % 4 === 0) && (ano % 100 !== 0)
+    return cond1 || cond2
+ }
+ console.log(checaAnoBissexto(2004))
